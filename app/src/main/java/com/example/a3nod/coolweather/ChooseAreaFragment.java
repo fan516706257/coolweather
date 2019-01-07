06a35cbba35aebd7fb4cf2868d12d1de4a07401c
fan516706257 <fanlanlong@163.com>
2019-01-07 15:48:47 +0800

    加入显示天气信息的功能

--- app/src/main/java/com/example/a3nod/coolweather/ChooseAreaFragment.java
+++ app/src/main/java/com/example/a3nod/coolweather/ChooseAreaFragment.java
@@ -1,8 +1,10 @@
 package com.example.a3nod.coolweather;
 
 import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -85,6 +87,13 @@ public class ChooseAreaFragment extends Fragment {
                 }else if (currentLevel == LEVEL_CITY){
                     selectedCity = cityList.get(position);
                     queryCounties();
+                }else if (currentLevel == LEVEL_COUNTY){
+                    String weatherId = countyList.get(position).getWeatherId();
+                    Intent intent = new Intent(getActivity(),WeatherActivity.class);
+                    Log.d("Debug", "weatherId: " + weatherId);
+                    intent.putExtra("weather_id",weatherId);
+                    startActivity(intent);
+//                    getActivity().finish();
                 }
             }
         });
